c61ae62435d50f13c0fd0c70f4f0bd9614bfb929
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-10-17 11:43:13 +0200

    lavoro in corso

---
+++ s4-buildweek/assets/js/fb.js
@@ -0,0 +1,29 @@
+
+
+function resetCommentField() {
+    const commentField = document.querySelector('input[name="text"]');
+    if (commentField) {
+        commentField.value = ''; 
+    }
+}
+
+
+function resetStarRating() {
+    const starInputs = document.querySelectorAll('input[name="star"]');
+    if (starInputs) {
+        starInputs.forEach(input => {
+            input.checked = false; 
+        });
+    }
+}
+
+const resetButton = document.getElementById('button');
+if (resetButton) {
+    resetButton.addEventListener('click', function () {
+        resetCommentField(); 
+        resetStarRating(); 
+        alert("Grazie per il FeedBack");
+    });
+} 
+
+
