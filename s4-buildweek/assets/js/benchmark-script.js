1a592eed9941a00ef38738be1e09c601e7e4a3aa
Emilio-Plances <emilio.plances@gmail.com>
2023-10-18 14:07:52 +0200

    pagina benchmark completata con zoom 100% e non 75

--- s4-buildweek/assets/js/benchmark-script.js
+++ s4-buildweek/assets/js/benchmark-script.js
@@ -1,3 +1,4 @@
+let containerBenchmark=document.querySelector(`.container-benchmark`)
 let countdownNumber = document.querySelector('.countdown-number span');
 let reset=document.querySelector(`button`);
 let circle=document.querySelector(`circle`);
@@ -5,22 +6,17 @@ let domanda=document.querySelector(`.domanda`);
 let nextQuestion=document.querySelector(`.next-question`);
 let risposte=document.querySelector(`.risposte`);
 let questionCounter=document.querySelector(`.question-counter p`);
+let risposteEsatte=0;
 let countdown = 60;
 
 countdownNumber.innerText = countdown;
 reset.addEventListener(`click`,()=>{
-  countdown=61;
+  countdown=60;
   circle.getAnimations().forEach((anim) => {
     anim.cancel();
     anim.play();
   });
 })
-
-setInterval(function() {
-  countdown = --countdown < 0 ? 60 : countdown;
-  countdownNumber.innerText = countdown;
-}, 1000);
-
 
 /**************************************************+
  *  Sezione domande
@@ -30,12 +26,19 @@ fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=easy`)
 .then(res=>res.json())
 .then(res=>{
   let domande=res.results;
-  
   let i=0;
+
+  if(i<10){
+    setInterval(function() {
+      countdown = --countdown < 0 ? 60 : countdown;
+      countdownNumber.innerText = countdown;
+    }, 1000);
+  }
+ 
+
+  
   console.log(domande);
   next(i,domande);
-  
-
   setInterval(function() {
     if(countdown==0){
       i++;
@@ -44,32 +47,34 @@ fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=easy`)
   }, 1000);
 
   domanda.innerHTML=domande[i].question;
-  
-  
-  
+
   nextQuestion.addEventListener(`click`,()=>{
-    i++;
+    i++; 
+    
     countdown=61;
-    i=next(i,domande);
-    console.log(i);
+    next(i,domande);
+    console.log(risposteEsatte);
+    if(i==10){
+      containerBenchmark.remove();
+      return;
+    } 
   })
 })
 
-
 function next(i,myArray) {
-  let newArray=[...myArray[i].incorrect_answers,myArray[i].correct_answer];
-
-  //controlloRisposta(myArray);
-
+  let newArray=[];
+  controlloRisposta(myArray,i);
   if(i>=myArray.length)
     return;
-  
+  if(i<=myArray.length){
+    newArray=[...myArray[i].incorrect_answers,myArray[i].correct_answer];
+  }
+
   questionCounter.innerHTML=`Domanda ${i+1} <span>/${myArray.length}</span>`;
 
   domanda.innerHTML=myArray[i].question;
   newArray.sort();
 
-  console.dir(risposte);
   while(risposte.firstChild){
     risposte.removeChild(risposte.firstChild);
   }
@@ -79,13 +84,8 @@ function next(i,myArray) {
     risposta.classList.add(`risposta`);
     risposta.innerHTML=element;
     risposte.append(risposta);
-
     rispostaButton(risposta,risposte);
-    
   })
-  
-  
-  return i;
 }
 
 function rispostaButton(risposta,risposte) {
@@ -99,13 +99,19 @@ function rispostaButton(risposta,risposte) {
   
 }
 
-function controlloRisposta(myArray){
-
-  let rispostaData = Array.from(risposte.childNodes).filter(element=>{
-    return element.style.backgroundColor==`#900080`;
+function controlloRisposta(myArray,i){
+  
+  let rispostaData=`     `;
+  Array.from(risposte.childNodes).forEach(element=>{
+     if(element.style.backgroundColor==`rgb(144, 0, 128)`){
+      rispostaData=element;
+     }
   })
   
-  console.log(rispostaData);
+  if(i>0 && i<=10){
+    if(myArray[i-1].correct_answer == rispostaData.innerText){
+      risposteEsatte++
+    }
+  }
 }
-
 
