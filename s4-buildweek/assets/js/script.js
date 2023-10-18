dc537673fa4d7639574fad3751db7ac76610fcaf
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-10-18 21:51:32 +0200

    result in corso

--- s4-buildweek/assets/js/script.js
+++ s4-buildweek/assets/js/script.js
@@ -1,3 +1,32 @@
+//donut
+
+const risultati = []
+risultati[0]= 20
+risultati[1]=80
+
+const myChart = document.querySelector('.my-chart');
+new Chart(myChart, {
+        type : 'doughnut',
+        data : {
+            labels : ['Wrong', 'Correct'],
+            datasets : [
+                {
+                data : risultati,
+                backgroundColor: ['rgb(194 18 141)' , 'rgb(0 255 255)'],
+                borderColor: 'transparent',
+                }
+            ]
+        },
+        options:{
+            plugins:{
+                legend: {
+                    display: false
+                }
+            },
+            cutout: 135
+        },
+
+ });
 
 const checkbox = document.querySelector('.checkbox');
 const button = document.querySelector('.button');
