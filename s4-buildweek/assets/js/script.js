6f545df38c4710930fa542d1874ff83852ab5c26
Emilio-Plances <emilio.plances@gmail.com>
2023-10-18 16:58:34 +0200

    Welcome (Completato 100%)

--- s4-buildweek/assets/js/script.js
+++ s4-buildweek/assets/js/script.js
@@ -1,11 +1,10 @@
-
 const checkbox = document.querySelector('.checkbox');
 const button = document.querySelector('.button');
 
 checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {button.disabled = false; 
-        button.classList.remove ('disabled');
-    }else {button.disabled=true;
-        button.classList.add ('disabled')};
+    if(checkbox.checked){
+        button.classList.add ('abled');
+    }else{ 
+        button.classList.remove ('abled')};
 });
 
