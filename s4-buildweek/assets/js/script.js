103f214cc37fa40c8e0d8369b6c01643268d6332
vincenzo8825 <vincenzorocca88@gmail.com>
2023-10-19 15:50:24 +0200

    ffff

--- s4-buildweek/assets/js/script.js
+++ s4-buildweek/assets/js/script.js
@@ -1,11 +1,33 @@
 
-const checkbox = document.querySelector('.checkbox');
-const button = document.querySelector('.button');
+document.addEventListener("DOMContentLoaded", function () {
+    // Dati per il grafico a torta (personalizza questi dati con i tuoi valori)
+    var correctPercentage = 66.7;
+    var wrongPercentage = 33.3;
 
-checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {button.disabled = false; 
-        button.classList.remove ('disabled');
-    }else {button.disabled=true;
-        button.classList.add ('disabled')};
+    var ctx = document.getElementById('myPieChart').getContext('2d');
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Correct', 'Wrong'],
+            datasets: [{
+                data: [correctPercentage, wrongPercentage],
+                backgroundColor: ['green', 'red'],
+            }]
+        },
+    });
+
+    // Gestisci l'apertura della finestra
+    var modal = document.getElementById('modal');
+    var rateButton = document.getElementById('rateButton');
+
+    rateButton.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    // Chiudi la finestra quando si fa clic al di fuori di essa
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
-
